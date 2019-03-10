92b8865e7857658b1271e9dcb9ecedf4b4a49cbe
Mike Ajala <agmt5989@gmail.com>
2019-03-10 09:16:49 +0100

    up with the components

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,6 +5,12 @@ var ingredients = [{'id': 1, 'text': 'ham'}, {'id': 2, 'text': 'cheese'}, {'id':
 
 let List = React.createClass({
   render: function() {
-    
+    let listItems = ingredients.map(function (item) {
+	    return <ListItem key={item.id} ingredient={item.text} />;
+    })
+
+	  return <ul>{listItems}</ul>;
   }
-})
+});
+
+
